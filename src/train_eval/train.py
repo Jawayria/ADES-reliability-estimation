481a78bd0fab76eee87723f073e58d967c58cd89
EinarLop <einarlop0@gmail.com>
2025-01-30 09:39:38 +0200

    Class Imbalance Configuration

--- src/train_eval/train.py
+++ src/train_eval/train.py
@@ -7,6 +7,10 @@ def train(model, train_loader, val_loader, train_config, model_file_path):
     best_val_loss = float('inf')
     patience_counter = 0
 
+    training_losses = []
+    validation_losses = []
+
+
     for epoch in range(num_epochs):
         # Training phase
         model.train()
@@ -27,6 +31,8 @@ def train(model, train_loader, val_loader, train_config, model_file_path):
         avg_train_loss = total_train_loss / len(train_loader)
         print(f"Epoch {epoch + 1}/{num_epochs}, Train Loss: {avg_train_loss:.4f}")
 
+        training_losses.append(avg_train_loss)
+
         # Validation phase
         model.eval()
         total_val_loss = 0
@@ -44,6 +50,8 @@ def train(model, train_loader, val_loader, train_config, model_file_path):
         avg_val_loss = total_val_loss / len(val_loader)
         print(f"Epoch {epoch + 1}/{num_epochs}, Validation Loss: {avg_val_loss:.4f}")
 
+        validation_losses.append(avg_val_loss)
+
         # Early stopping logic based on validation loss
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
@@ -59,3 +67,5 @@ def train(model, train_loader, val_loader, train_config, model_file_path):
             print("Early stopping triggered.")
             break
 
+
+    return training_losses, validation_losses

c19b2c82280d038720a67e993956803345854294
Jawayria Hashmi <jawayriahashmi@gmail.com>
2025-02-06 20:42:38 +0200

    Added node type count features and updates failure rate computation

--- src/datamanip/feature_extraction.py
+++ src/datamanip/feature_extraction.py
@@ -8,18 +8,25 @@ from topology_parameters import prefix_to_type, type_failure_rates
 from filepath import configs_list_path
 
 
-def calculate_failure_rates() -> dict[int, float]:
+def calculate_failure_rates(config_id) -> dict[int, float]:
     # Initialize the dictionary to store the index to type mapping
     index_to_type = {}
 
-    # Read the first line from 'configs.txt'
+    search_string = f"config_{config_id}"
+    # Read the config from 'configs.txt'
     with open(configs_list_path, 'r') as file:
-        first_line = file.readline().strip()
+        for line in file:
+            if line.startswith(search_string):
+                selected_line = line.strip()
+                break 
+            else:
+                selected_line = None  
+
 
     # Extract the part after 'config_0: [' and before the closing ']'
-    start = first_line.find('[')
-    end = first_line.find(']')
-    node_list_str = first_line[start + 1:end]
+    start = selected_line.find('[')
+    end = selected_line.find(']')
+    node_list_str = selected_line[start + 1:end]
 
     # Split the node names by comma and strip whitespace
     node_names = [node.strip() for node in node_list_str.split(',')]
@@ -41,7 +48,43 @@ def calculate_failure_rates() -> dict[int, float]:
         index_to_failure_rate[index] = failure_rate
 
     return index_to_failure_rate
+
+
+def calculate_type_count(config_id) -> dict[str, int]:
     
+    search_string = f"config_{config_id}"
+    type_count = {'slave':0, 'switch':0, 'link':0, 'i':0}
+    # Read the first line from 'configs.txt'
+    with open(configs_list_path, 'r') as file:
+        for line in file:
+            if line.startswith(search_string):
+                selected_line = line.strip()
+                break 
+            else:
+                selected_line = None  
+
+    # Extract the part after 'config_0: [' and before the closing ']'
+    start = selected_line.find('[')
+    end = selected_line.find(']')
+    node_list_str = selected_line[start + 1:end]
+
+    # Split the node names by comma and strip whitespace
+    node_names = [node.strip() for node in node_list_str.split(',')]
+
+    # Iterate over the node names and map index to type
+    for index, node_name in enumerate(node_names):
+        if node_name.startswith('Sw'):
+            node_type = 'switch'
+        else:
+            # Use the first character as prefix
+            prefix = node_name[0]
+            node_type = prefix_to_type.get(prefix, 'unknown')
+            
+        type_count[node_type] = type_count[node_type] + 1
+
+    return type_count
+
+
 def extract_features_from_data(merged_df_exploded: pd.DataFrame) -> list[list[list[Union[float, int]]]]:
     all_node_features: list[list[list[Union[float, int]]]] = []
     
@@ -65,26 +108,36 @@ def extract_features_from_data(merged_df_exploded: pd.DataFrame) -> list[list[li
         k_core = nx.core_number(G)
         degree = adj_matrix.sum(axis=1)
 
-        failure_rates = calculate_failure_rates()
         
+        failure_rates = calculate_failure_rates(row['config_id'])
+        type_count = calculate_type_count(row['config_id'])
+        switch_count = type_count['switch']
+        slave_count = type_count['slave']
+        link_count = type_count['link']
+        i_count = type_count['i']
+
         mean_failure_rate_neighbors = {
-            node: np.mean([failure_rates.get(n, 0.0) for n in G.neighbors(node)]) 
+            node: np.mean([failure_rates.get(n, 0.0) for n in G.neighbors(node)])
             if len(list(G.neighbors(node))) > 0 else 0.0
             for node in G.nodes
         }
-        
+
         node_features_of_one_graph = []
         for node in G.nodes():
             node_features_of_one_node = [
-                degree_centrality[node], 
-                closeness_centrality[node], 
-                degree[node], 
+                degree_centrality[node],
+                closeness_centrality[node],
+                degree[node],
                 timestamp,
+                switch_count,
+                slave_count,
+                link_count,
+                i_count,
                 failure_rates.get(node, 0.0),
-                betweenness_centrality[node], 
+                betweenness_centrality[node],
                 eigenvector_centrality[node],
-                pagerank[node], 
-                k_core[node], 
+                pagerank[node],
+                k_core[node],
                 mean_failure_rate_neighbors[node]
             ]
             node_features_of_one_graph.append(node_features_of_one_node)

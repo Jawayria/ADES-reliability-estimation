d0a8723beb08a97e62aa9432209b6fb2ddc3de3c
EinarLop <einarlop0@gmail.com>
2025-04-07 15:29:36 +0200

    Retraining of all models to use GAT + Attention Heads

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,10 @@ import pandas as pd
 import torch
 
 from filepath import hyperparameters_path, best_models_path, model_checkpoints_path
+import models
+import importlib
+
+importlib.reload(models)
 from models.GAT import GAT
 from models.GAT_LN_HEAD import GAT_LN_HEAD
 
@@ -23,14 +27,17 @@ def load_best_model_based_on_match(match : str) -> GAT:
     NODE_FEATURES = int(first_row["Node features"])
     HIDDEN_DIM = int(first_row["Hidden dimension"])
     DROPOUT_RATE = float(first_row["Dropout rate"])
-
     # Initialize the GAT model with extracted parameters
-    if match == "1-1":
-        print("Using GAT_LN_HEAD for match 1-1")
+    if match == "1-1" or match == "2-1" or   match == "2-2" or match == "3-2" or match == "3-3" or match == "3-4":
+        print(f"Using GAT_LN_HEAD for match {match}")
         model = GAT_LN_HEAD(input_dim=NODE_FEATURES, hidden_dim=HIDDEN_DIM, output_dim=2, dropout_rate=DROPOUT_RATE)
+        model.load_state_dict(torch.load(os.path.join(best_models_path, f"{match}.pth"), weights_only=True))
     else:
+        print(f"Using GAT for match {match}")
         model = GAT(input_dim=NODE_FEATURES, hidden_dim=HIDDEN_DIM, output_dim=2, dropout_rate=DROPOUT_RATE)
-    model.load_state_dict(torch.load(os.path.join(best_models_path, f"{match}.pth"), weights_only=True))
+        model.load_state_dict(torch.load(os.path.join(best_models_path, f"{match}.pth"), weights_only=True))
+
+
     return model
 
 

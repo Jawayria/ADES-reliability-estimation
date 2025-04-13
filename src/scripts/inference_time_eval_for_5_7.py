Fixed inference time calculation not taking node features into account

src/scripts/inference_time_eval_for_5_7.py
@@ -10,6 +10,7 @@ from torch_geometric.data import Data
 
 from datamanip.dataset_parts_construction import construct_edge_indices
 from datamanip.feature_extraction import extract_features_from_data
+from datamanip.read_csvs import read_matrix_file
 from filepath import dataset_path
 from train_eval.predict import predict_one_ensemble
 from utils import load_best_model_based_on_match
@@ -27,39 +28,7 @@ def extract_config_id(path: str) -> int:
         raise ValueError(f"Could not extract config_id from path: {path}")
 
 
-def read_matrix_file(path: str) -> list:
-    """
-    Read a single matrix file and return the matrix as a list of lists of ints.
-    Assumes the file contains a string representation of a matrix with square brackets.
-    """
-    with open(path, 'r') as f:
-        content = f.read()
 
-    # Remove the first and last character ('[' and ']')
-    content = content[1:-1]
-    string_list = []
-    current_row = ''
-    start_line = False
-
-    for c in content:
-        if c == '[':
-            start_line = True
-        elif c == ']':
-            start_line = False
-            string_list.append(current_row)
-            current_row = ''
-        elif start_line:
-            if c != '\n':
-                current_row += c
-
-    # Convert the string representation into a list of lists of ints
-    matrix = []
-    for string in string_list:
-        # Replace '.' with a space, split by whitespace, and convert to int
-        vals = string.replace('.', ' ').split()
-        vals = [int(v) for v in vals]
-        matrix.append(vals)
-    return matrix
 
 
 def read_reliability_file(path: str) -> pd.DataFrame:
@@ -113,20 +82,21 @@ def calc_inference_times():
     ensemble = {}
     for match in matches:
         ensemble[match] = load_best_model_based_on_match(match)
-    config = 1003
-    start_time = perf_counter()
-    dict = {'matrix': np.array(read_matrix_file(f'../../data/5-switches-7-slaves/matrix/config_{config}.txt')),
-            'config_id': config, 'timestamp': 1000}
-    matrices_df = pd.DataFrame([dict])
-    df = matrices_df
-    node_features = torch.Tensor(extract_features_from_data(df))
-    edge_indices = construct_edge_indices(df)
-    data = Data(x=node_features[0], edge_index=edge_indices[0], y=100)
-    predicted_class = predict_one_ensemble(ensemble, data)
-    end_time = perf_counter()
-    inference_time = end_time - start_time
-    print("Best Inference Time:", inference_time)
-    print("Predicted Class:", predicted_class)
+        ensemble[match].eval()
+    configs = range(1001,1008)
+    for config in configs:
+        start_time = perf_counter()
+        dict = {'matrix': np.array(read_matrix_file(f'../../data/5-switches-7-slaves/matrix/config_{config}.txt')),
+                'config_id': config, 'timestamp': 1000}
+        matrices_df = pd.DataFrame([dict])
+        df = matrices_df
+        node_features = torch.Tensor(extract_features_from_data(df))
+        edge_indices = construct_edge_indices(df)
+        data = Data(x=node_features[0], edge_index=edge_indices[0], y=100)
+        predicted_class = predict_one_ensemble(ensemble, data)
+        end_time = perf_counter()
+        inference_time = end_time - start_time
+        print(f"config_{config}: inference time: {inference_time}, predicted class: {predicted_class}")
 
 
 if __name__ == "__main__":

src/scripts/inference_times_approx.py
@@ -1,23 +1,24 @@
 # Import necessary libraries
+import glob
+import os
+
 import numpy as np
+import pandas as pd
 from time import perf_counter
 
 import torch
-from torch_geometric.loader import DataLoader
+from torch_geometric.data import Data
+from tqdm import tqdm
 
-
-from datamanip.datasetmanip.three_five_dataset import ThreeFiveDataset
-from filepath import dataset_path
+from datamanip.dataset_parts_construction import construct_edge_indices
+from datamanip.feature_extraction import extract_features_from_data
+from datamanip.read_csvs import read_matrix_file
+from filepath import matrices_path
 from train_eval.predict import predict_one_ensemble
 from utils import load_best_model_based_on_match
 
 
 def calc_inference_times():
-    # Convert dataset to PyTorch tensors
-    dataset = ThreeFiveDataset(dataset_path, 'ensemble', "test")
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
-    NODE_FEATURES = 12
-    DROPOUT_RATE = 0.3
     # Measure inference times
     inference_times = []
     total_time = 0
@@ -28,8 +29,21 @@ def calc_inference_times():
         ensemble[match] = load_best_model_based_on_match(match)
 
     with torch.no_grad():
-        for data in dataloader:
+        # Check each file in matrices_path and create a Data object for each
+        #Get list of all files in the directory
+        files = sorted(glob.glob(matrices_path))
+        for file in tqdm(files):
+            config_id = os.path.basename(file).split('_')[1].split('.')[0]
+            if (config_id == "1"):
+                continue
+            matrix = read_matrix_file(os.path.join(matrices_path, file))
             start_time = perf_counter()
+            dict = {'matrix': np.array(matrix),
+                    'config_id': config_id, 'timestamp': 1000}
+            df = pd.DataFrame([dict])
+            node_features = torch.Tensor(extract_features_from_data(df))
+            edge_indices = construct_edge_indices(df)
+            data = Data(x=node_features[0], edge_index=edge_indices[0], y=100)
             _ = predict_one_ensemble(ensemble, data)
             end_time = perf_counter()
             inference_time = end_time - start_time
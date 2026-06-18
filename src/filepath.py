9bc0768ab1f12b971ff1345722cf426acc2c22c9
GesPavel <me@pavelges.com>
2026-06-18 07:42:26 +0300

    Add files from full dataset (not reduced to 10k hours), also update paths.py to point to correct dataset

--- src/filepath.py
+++ src/filepath.py
@@ -9,7 +9,7 @@ hyperparameters_path = os.path.join(model_checkpoints_path, "booster", "best_mod
 best_models_path = os.path.join(model_checkpoints_path, "booster", "best_models")
 meta_learner_model_path = os.path.join(model_checkpoints_path, "meta_learner")
 
-dataset_path = os.path.join(THIS_DIR, "..", "data", "3-switches-5-slaves")
+dataset_path = os.path.join(THIS_DIR, "..", "data", "3-switches-5-slaves-10khours")
 matrices_path = os.path.join(dataset_path, "raw", "01_system", "matrix", "config_*.txt")
 reliabilities_path = os.path.join(dataset_path, "raw", "03_reliability", "hours", "config_*.csv")
 configs_list_path = os.path.join(dataset_path, "raw", "01_system", "configs.txt")
